5255b483b95266be805468810610fedc7d409fa1
Cleberson Saller <103286833+csaller-encora@users.noreply.github.com>
2022-10-28 13:16:51 -0300

    moved Jinja script to root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ${#} -eq 0 ]; then
   echo "Starting docker-nginx"
 
-  j2 --undefined /code/templates/nginx.conf.j2 > /etc/nginx/nginx.conf
+  j2 --undefined /code/nginx.conf.j2 > /etc/nginx/nginx.conf
 
   echo "Starting nginx"
   exec nginx -g 'daemon off;'
